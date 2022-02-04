9b78177550c17a12d93b3ed0c42ab013dd1c3f64
MillennialBug <MillennialDebug@gmail.com>
2022-02-04 10:55:51 +0000

    Draft component layout

--- GraphicalProgrammingLanguage/GraphicalProgrammingLanguage/Form1.Designer.cs
+++ GraphicalProgrammingLanguage/GraphicalProgrammingLanguage/Form1.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.run_button = new System.Windows.Forms.Button();
+            this.save_button = new System.Windows.Forms.Button();
+            this.load_button = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(360, 851);
+            this.textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(775, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1240, 1240);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 1199);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(288, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // run_button
+            // 
+            this.run_button.Location = new System.Drawing.Point(12, 897);
+            this.run_button.Name = "run_button";
+            this.run_button.Size = new System.Drawing.Size(106, 59);
+            this.run_button.TabIndex = 3;
+            this.run_button.Text = "Run";
+            this.run_button.UseVisualStyleBackColor = true;
+            this.run_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // save_button
+            // 
+            this.save_button.Location = new System.Drawing.Point(141, 897);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(111, 59);
+            this.save_button.TabIndex = 4;
+            this.save_button.Text = "Save";
+            this.save_button.UseVisualStyleBackColor = true;
+            // 
+            // load_button
+            // 
+            this.load_button.Location = new System.Drawing.Point(258, 897);
+            this.load_button.Name = "load_button";
+            this.load_button.Size = new System.Drawing.Size(114, 59);
+            this.load_button.TabIndex = 5;
+            this.load_button.Text = "Load";
+            this.load_button.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox3.Location = new System.Drawing.Point(396, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(360, 851);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(2027, 1264);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.load_button);
+            this.Controls.Add(this.save_button);
+            this.Controls.Add(this.run_button);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button run_button;
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.Button load_button;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
